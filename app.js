7ad922e10ec2be412b76443979bafb6480aed050
Huu Phat <huuphat1908@gmail.com>
2021-06-15 10:03:18 +0700

    Add commnet on app.js

--- app.js
+++ app.js
@@ -14,11 +14,14 @@ const app = express();
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
+// middleware installed
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//middleware custom
 
 // connect to database
 db.mongoose.connect(db.url, {
